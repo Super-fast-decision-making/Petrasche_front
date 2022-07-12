0746ffa74d3510faa0ed2227a24b53bb4011a5d3
앵가리 <>
2022-07-12 17:09:18 +0900

    feat: 회원정보 버튼 활성화

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -1,14 +1,29 @@
 // 내 게시물 불러오기(전체)
 async function loadMyArticle() {
     articles = await getMyArticle()
+    console.log(articles)
     for(let i=0; i<articles.length; i++) {
         let image = articles[i].images[0]
-        console.log(image)
-        let article_box = document.getElementById("article_box")
+        const article_box = document.getElementById("article_box")
         article_box.innerHTML +=
             `<div class="article_card">
-                <img class="article_img" src='${image}'/>
+                <img src='${image}'/>
             </div>`
+    }
+}
+
+function changeButton() {
+    const article_box = document.getElementById("article_box")
+    const menu_change_button = document.getElementById("menu_change_button")
+
+    if (menu_change_button.innerText == "회원정보") {
+        // let user = getUserInfo()
+        // menu_change_button.setAttribute("onclick", "changeButton("+{}+")")
+        article_box.style.display = "none"
+        menu_change_button.innerText = "게시물 보기"
+    } else {
+        article_box.style.display = "flex"
+        menu_change_button.innerText = "회원정보"
     }
 }
 
