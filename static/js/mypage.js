14ee70e334e11a59b959ab3b307cd05e1ee82757
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-13 17:35:55 +0900

    add 좋아요 페이지 연동

--- static/js/mypage.js
+++ static/js/mypage.js
@@ -22,18 +22,21 @@ function changeButton() {
     const user_info_box = document.getElementById("user_info_box")
     const pet_select_box = document.getElementById("pet_select_box")
     const menu_change_button = document.getElementById("menu_change_button")
+    const like_article_box = document.getElementById("like_article_box")
     
 
     if (menu_change_button.innerText == "프로필 변경") {
         showUserInfo()
         article_box.style.display = "none"
         pet_select_box.style.display = "none"
+        like_article_box.style.display = "none"
         user_info_box.style.display = "flex"
         menu_change_button.innerText = "게시물 보기"
     } else {
         article_box.style.display = "flex"
         pet_select_box.style.display = "flex"
         user_info_box.style.display = "none"
+        like_article_box.style.display = "none"
         menu_change_button.innerText = "프로필 변경"
     }
 }
