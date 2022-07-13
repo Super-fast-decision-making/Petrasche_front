08880d4c379afafe301f09bef29e82f03e25e31b
joohuun <k00005781@gmail.com>
2022-07-13 09:49:34 +0900

    충돌 해결

--- static/js/api.js
+++ static/js/api.js
@@ -3,50 +3,50 @@ const frontend_base_url = "http://127.0.0.1:5500/"
 
 
 //회원가입
-async function handleSignup(){
+async function handleSignup() {
     const signupData = {
         email: document.getElementById("email").value,
         username: document.getElementById("username").value,
-        password:document.getElementById("password").value,
-        birthday_date:document.getElementById("birthday").value,
-        is_active_val:document.getElementById("is_active").value,
-        gender_choice:document.getElementById("gender").value,
+        password: document.getElementById("password").value,
+        birthday_date: document.getElementById("birthday").value,
+        is_active_val: document.getElementById("is_active").value,
+        gender_choice: document.getElementById("gender").value,
     }
     console.log(signupData)
-    const response = await fetch(`${backend_base_url}user/`,{
-        headers:{
-            Accept:'application/json',
-            'Content-type':'application/json'
+    const response = await fetch(`${backend_base_url}user/`, {
+        headers: {
+            Accept: 'application/json',
+            'Content-type': 'application/json'
         },
-        method:'POST',
-        body:JSON.stringify(signupData)
+        method: 'POST',
+        body: JSON.stringify(signupData)
     })
-    response_json=await response.json()
+    response_json = await response.json()
     print(response_json)
 
-    if (response.status==200) {
+    if (response.status == 200) {
         window.location.replace(`${frontend_base_url}login.html`);
-    }else{
-        alert(response_json['username'],response_json['email'],response_json['password'], response_json['error'])
+    } else {
+        alert(response_json['username'], response_json['email'], response_json['password'], response_json['error'])
     }
 }
 
 //로그인
 async function handleLogin() {
-    const loginData={
-        email:document.getElementById("email").value,
-        password:document.getElementById("password").value        
+    const loginData = {
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
     }
     console.log(loginData)
-    const response=await fetch(`${backend_base_url}user/login/`, {
-        headers:{
+    const response = await fetch(`${backend_base_url}user/login/`, {
+        headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        method:'POST',
-        body:JSON.stringify(loginData)
+        method: 'POST',
+        body: JSON.stringify(loginData)
     })
-    response_json=await response.json()
+    response_json = await response.json()
 
     if (response.status == 200) {
         localStorage.setItem("user_access_token", response_json.access)
