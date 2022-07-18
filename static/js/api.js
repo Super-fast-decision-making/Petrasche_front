3c41159fb6af04a3dc09ef83a398b545558095a9
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-07-18 14:03:47 +0900

    fix 카카오 로그인/ 회원가입 api 연동 완료

--- static/js/api.js
+++ static/js/api.js
@@ -2,6 +2,8 @@ const backend_base_url = "http://127.0.0.1:8000/"
 const frontend_base_url = "http://127.0.0.1:5500/"
 
 
+
+
 //회원가입
 async function handleSignup(){
     const gender_check = document.querySelectorAll("input[name=gender]:checked");
