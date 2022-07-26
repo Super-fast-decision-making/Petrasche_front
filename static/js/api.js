98371b43916be1980a7f5930a373bc1493b88b70
SG-Marco <rollingpoe@gmail.com>
2022-07-26 10:53:48 +0900

    enhancement - urls.py에 url들 모아놓고 나머지 js에서는 삭제.

--- static/js/api.js
+++ static/js/api.js
@@ -1,5 +1,3 @@
-const backend_base_url = "http://127.0.0.1:8000/"
-const frontend_base_url = "http://127.0.0.1:5500/"
 
 
 // 회원가입
--- static/js/hashtag.js
+++ static/js/hashtag.js
@@ -1,7 +1,3 @@
-// const BACK_END_URL = "http://127.0.0.1:8000/article/";
-// const USER_URL = "http://127.0.0.1:8000/user/";
-// const backend_base_url = "http://127.0.0.1:8000/"
-// const frontend_base_url = "http://127.0.0.1:5500/"
 
 
 function tagToLink(str){
