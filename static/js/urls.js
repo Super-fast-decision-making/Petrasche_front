3acade2531ed44a12bec58793cfac0ffd44c8f4c
한예슬 Yeseul Han <104334219+Madung2@users.noreply.github.com>
2022-08-02 13:09:28 +0900

    Update urls.js

--- static/js/urls.js
+++ static/js/urls.js
@@ -1,5 +1,5 @@
-const backend_base_url = "http://127.0.0.1:8000/"
-// const backend_base_url = "http://3.39.219.239/"
+// const backend_base_url = "http://127.0.0.1:8000/"
+const backend_base_url = "http://3.39.219.239/"
 const frontend_base_url = "http://127.0.0.1:5500/"
 
 
