effa260a02e07a4722a656be5bec595d22b2f7ed
min_fs <103223878+mingoodjob@users.noreply.github.com>
2022-08-09 20:21:46 +0900

    Update urls.js

--- static/js/urls.js
+++ static/js/urls.js
@@ -4,7 +4,7 @@ const frontend_base_url = "https://www.petrasche.com/"
 const backend_base_url = "https://www.petrasche.site/"
 // const backend_base_url = "http://3.39.219.239/"
 // const ws_base_url = "wss://3.39.219.239:8001/"
-const ws_base_url = "wss://www.petrasche.site:8001/"
+const ws_base_url = "ws://www.petrasche.site:8001/"
 
 
 async function profile(user_id) {
