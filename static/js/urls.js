eb24fda2d2f6cfc64e12f9a5cb980bc48eeebe30
한예슬 Yeseul Han <104334219+Madung2@users.noreply.github.com>
2022-08-09 19:47:29 +0900

    Update urls.js

--- static/js/urls.js
+++ static/js/urls.js
@@ -3,7 +3,7 @@
 const frontend_base_url = "https://www.petrasche.com/"
 const backend_base_url = "https://www.petrasche.site/"
 // const backend_base_url = "http://3.39.219.239/"
-const ws_base_url = "ws://3.39.219.239:8001/"
+const ws_base_url = "wss://3.39.219.239:8001/"
 
 
 async function profile(user_id) {
