0cefa333aedaa790affa61e6078a1df930112670
예슬_노트북\tulip <tuliphan91@gmail.com>
2022-08-09 13:42:29 +0900

    fix 산책페이지 지도 모달 보이게 수정, 산책 페이지 모든 버튼에 포인터커서 추가,

--- static/js/urls.js
+++ static/js/urls.js
@@ -1,8 +1,8 @@
-// const backend_base_url = "http://127.0.0.1:8000/"
+const backend_base_url = "http://127.0.0.1:8000/"
 const frontend_base_url = "http://127.0.0.1:5500/"
 // const frontend_base_url = "http://petrasche.com/"
 
-const backend_base_url = "http://3.39.219.239/"
+// const backend_base_url = "http://3.39.219.239/"
 const ws_base_url = "ws://3.39.219.239:8001/"
 
 
