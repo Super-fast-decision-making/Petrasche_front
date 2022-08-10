6b74962541344e3e5b36a5292d9bb014443a0e24
이민기 <psjlmk@gmail.com>
2022-08-10 13:15:29 +0900

    fix 검색 헤더값 auth 보내기

--- static/js/search.js
+++ static/js/search.js
@@ -1,21 +1,29 @@
 async function search(div) {
-    let words_for_search = div.parentNode.childNodes[1].value
-    if (words_for_search.startsWith("#")) {
-      words_for_search = words_for_search.replace("#", "%23");
-    }
-      var url = new URL(backend_base_url + `article/search/?words=${words_for_search}`);
-    const search_results = await fetch(url)
-        .then(response => {
-            var status_code = response.status;
-            return Promise.resolve(response.json())
-                .then(data => ({ data, status_code }))
-        })
-
-  localStorage.setItem('search_results', JSON.stringify(search_results.data));
+  let words_for_search = div.parentNode.childNodes[1].value;
+  if (words_for_search.startsWith("#")) {
+    words_for_search = words_for_search.replace("#", "%23");
+  }
+  var url = new URL(
+    backend_base_url + `article/search/?words=${words_for_search}`
+  );
+  const search_results = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("user_access_token"),
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    var status_code = response.status;
+    return Promise.resolve(response.json()).then((data) => ({
+      data,
+      status_code,
+    }));
+  });
+  localStorage.setItem("search_results", JSON.stringify(search_results.data));
 
   if (search_results.status_code == 200) {
     window.location.replace(`${frontend_base_url}search_result.html`);
   } else {
-    alert(search_results.data.message)
+    alert(search_results.data.message);
   }
 }
